385af0a415a59dbc36d5e2ef62bbf0068479726c
Inhumannn <thomas.pnbm@gmail.com>
2025-05-23 11:46:53 +0200

    refactor(bars-menu): improve responsive menu
    - Changed display style to flex
    - Added position, background, margin,
      padding and gap styles
    - Improved responsiveness

--- js/bars-menu.js
+++ js/bars-menu.js
@@ -2,7 +2,13 @@ const barsMenu = document.querySelector("#bars");
 
 barsMenu.addEventListener("click", () => {
   const ulMenu = document.querySelector("#res-menu-ul");
-  ulMenu.style.display = ulMenu.style.display === "block" ? "none" : "block";
+  ulMenu.style.display = ulMenu.style.display === "flex" ? "none" : "flex";
+  ulMenu.style.position = "absolute";
+  ulMenu.style.background = "white";
+  ulMenu.style.margin = "-12px -86%";
+  ulMenu.style.padding = "12px 0";
+  ulMenu.style.gap = "20px";
+
   const navMenu = document.querySelector("#res-menu-nav");
   navMenu.style.display = "flex";
   navMenu.style.gap = "15px";
